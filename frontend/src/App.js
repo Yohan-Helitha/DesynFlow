70ed86cdfc2e1a2be5e407d1ed93f43bdfff56eb
kivindu-lakshan <kivindulakshan@gmail.com>
2025-09-19 14:21:59 +0530

    feat : created frontend enviorenment

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard_proc from "./Component/Dashboard_proc/Dashboard_proc";
+
+function App() {
+  return (
+    <div>
+      <Dashboard_proc>
+        
+      </Dashboard_proc>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Component/Dashboard_proc/Dashboard_proc.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import Nav from "../Nav/Nav"
+
+function Dashboard_proc() {
+  return (
+    <div>
+        <Nav/>
+      <h1>home</h1>
+    </div>
+  )
+}
+
+export default Dashboard_proc
