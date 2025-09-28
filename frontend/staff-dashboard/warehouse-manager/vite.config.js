e0844c36f044d8875aa317f75e278b3fc005f2a8
Yohan-Helitha <ceylongroupcentral@gmail.com>
2025-09-28 16:19:56 +0530

    update warehouse-manager for compatibility with main

--- frontend/staff-dashboard/warehouse-manager/vite.config.js
+++ frontend/staff-dashboard/warehouse-manager/vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    },
+    proxy: {
+      '/api': {
+        target: 'http://backend:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
